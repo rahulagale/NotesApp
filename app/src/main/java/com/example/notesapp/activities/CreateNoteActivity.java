c8bbec0f2b3ab72edfa81eff7a64f4769ec1080d
Rahul Agale <rahulagale60@gmail.com>
2022-02-23 11:11:32 +0530

    initial

---
+++ app/src/main/java/com/example/notesapp/activities/CreateNoteActivity.java
@@ -0,0 +1,29 @@
+package com.example.notesapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.notesapp.R;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    private Editable inputNoteTitle, inputNoteSubtitle, inputNoteText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
