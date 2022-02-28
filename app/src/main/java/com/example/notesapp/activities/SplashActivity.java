e282908bad169ed422c5e64075a4ea99288d7ee9
Rahul Agale <rahulagale60@gmail.com>
2022-02-28 14:20:40 +0530

    initial

---
+++ app/src/main/java/com/example/notesapp/activities/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.notesapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.example.notesapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN =2000;
+    Animation topAnim;
+    TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        appName = findViewById(R.id.tv_app_name);
+        appName.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
